cd0bb7b829cef75f225b05f94c67c4904344d8fc
Carlos Teixeira <1998carlosteixeira@gmail.com>
2021-12-01 13:53:11 +0000

    Day 1 finished

--- day1/index.js
+++ day1/index.js
@@ -1,7 +1,23 @@
-const fs = require('fs')
-  
-fs.readFile('data.txt', (err, data) => {
-    if (err) throw err;
-  
-    console.log(data.toString());
-})
+const fs = require("fs");
+let counterIncreased = 0;
+let data = fs.readFileSync("data.txt").toString().split("\n").map(Number);
+for (let index = 0; index < data.length; index++) {
+  const element = data[index];
+  if (element > data[index - 1]) {
+    counterIncreased++;
+  }
+}
+console.log("Part1: " + counterIncreased);
+
+counterIncreased = 0;
+for (let i = 2; i < data.length; i++) {
+  if (data[i + 1] !== undefined) {
+    const currentSum = data[i] + data[i - 1] + data[i - 2];
+    const nextSum = data[i] + data[i - 1] + data[i + 1];
+    if (currentSum < nextSum) {
+      counterIncreased++;
+    }
+  }
+}
+
+console.log("Part2: " + counterIncreased);
