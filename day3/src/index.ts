4ee81cdc632125d7458225932abe119c531cba4b
Carlos Teixeira <1998carlosteixeira@gmail.com>
2021-12-05 00:08:50 +0000

    update

--- day3/src/index.ts
+++ day3/src/index.ts
@@ -1,36 +1,36 @@
 import * as fs from "fs";
 import * as path from "path";
 
-// const part1 = async () => {
-//   let data: string[] = await fs
-//     .readFileSync(path.join(__dirname, "../files/data.txt"))
-//     .toString()
-//     .split("\n");
-//   let gammaRate: number[] = [];
-//   let epsilonRate: number[] = [];
-//   for (let index = 0; index < data[0].length; index++) {
-//     let countZeros: number = 0;
-//     let countOnes: number = 0;
-//     data.map(function (val, i) {
-//       val.split("").splice(index, 1).toString() == "0"
-//         ? countZeros++
-//         : countOnes++;
-//       if (i == data.length - 1) {
-//         gammaRate.push(countOnes > countZeros ? 1 : 0);
-//         epsilonRate.push(countOnes > countZeros ? 0 : 1);
-//       }
-//     });
-//   }
-//   return (
-//     parseInt(gammaRate.toString().replace(/,/g, ""), 2) *
-//     parseInt(epsilonRate.toString().replace(/,/g, ""), 2)
-//   );
-// };
-// console.log(
-//   part1().then((result) => {
-//     console.log("Parte1: " + result);
-//   })
-// );
+const part1 = async () => {
+  let data: string[] = await fs
+    .readFileSync(path.join(__dirname, "../files/data.txt"))
+    .toString()
+    .split("\n");
+  let gammaRate: number[] = [];
+  let epsilonRate: number[] = [];
+  for (let index = 0; index < data[0].length; index++) {
+    let countZeros: number = 0;
+    let countOnes: number = 0;
+    data.map(function (val, i) {
+      val.split("").splice(index, 1).toString() == "0"
+        ? countZeros++
+        : countOnes++;
+      if (i == data.length - 1) {
+        gammaRate.push(countOnes > countZeros ? 1 : 0);
+        epsilonRate.push(countOnes > countZeros ? 0 : 1);
+      }
+    });
+  }
+  return (
+    parseInt(gammaRate.toString().replace(/,/g, ""), 2) *
+    parseInt(epsilonRate.toString().replace(/,/g, ""), 2)
+  );
+};
+console.log(
+  part1().then((result) => {
+    console.log("Parte1: " + result);
+  })
+);
 
 const part2 = async () => {
   let oxygen: string[] = await fs
@@ -74,8 +74,6 @@ const part2 = async () => {
       });
     }
   }
-  console.log(oxygen);
-  // console.log(co);
   return parseInt(oxygen.toString(), 2) * parseInt(co.toString(), 2);
 };
 console.log(
